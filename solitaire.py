f0e55c126c83f880a10af93b4c464e95f794c5ed
Arthur B Guazzelli <arthur.b.guazzelli@gmail.com>
2023-01-20 15:10:44 -0300

    added "exit" command

--- solitaire.py
+++ solitaire.py
@@ -257,6 +257,9 @@ def take_from_stock(stock, waste, n=1):
 def parse_move(deck, tableau, stock, waste, foundations, move):
     # TODO: Move from foundation to tableau
     move = move.lower()
+    if move == "exit":
+        quit(0)
+
     if move == "reset":
         deck, tableau, stock, waste, foundations = reset_game()
         return deck, tableau, stock, waste, foundations
